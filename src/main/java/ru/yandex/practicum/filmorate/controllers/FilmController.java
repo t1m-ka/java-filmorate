237458022410b46d5b12cfd54699bb1329cd3090
Anatoly Mitapov <aa.mitapov@yandex.ru>
2023-04-30 11:04:48 +0800

    feat: добавить логирование

--- src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/FilmController.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.exceptions.ValidationException;
 
@@ -18,28 +19,32 @@ public class FilmController {
 
     @GetMapping
     public List<Film> getFilms() {
-
+        log.info("Получен запрос к эндпоинту: /GET /films");
         return films;
     }
 
     @PostMapping
     public Film createFilm(@Valid @RequestBody Film film) throws ValidationException {
+        log.info("Получен запрос к эндпоинту: /POST /films");
         if (film == null) {
             throw new ValidationException("Тело запроса не содержит объекта");
         }
         Film newFilm = film.toBuilder().id(filmId++).build();
         films.add(newFilm);
+        log.info("Фильм успешно создан, id = " + newFilm.getId() + ". Всего фильмов " + films.size());
         return newFilm;
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) throws ValidationException {
+        log.info("Получен запрос к эндпоинту: /PUT /films");
         if (film == null) {
             throw new ValidationException("Тело запроса не содержит объекта");
         }
         for (int i = 0; i < films.size(); i++) {
             if (films.get(i).getId() == film.getId()) {
                 films.set(i, film);
+                log.info("Фильм с id = " + film.getId() + " успешно обновлен.");
                 return film;
             }
         }
--- src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/UserController.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.model.exceptions.ValidationException;
@@ -10,17 +11,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     private final List<User> users = new ArrayList<>();
     private int userId = 1;
 
     @GetMapping
     public List<User> getUsers() {
+        log.info("Получен запрос к эндпоинту: /GET /users");
         return users;
     }
 
     @PostMapping
     public User createUser(@Valid @RequestBody User user) throws ValidationException {
+        log.info("Получен запрос к эндпоинту: /POST /users");
         if (user == null) {
             throw new ValidationException("Тело запроса не содержит объекта");
         }
@@ -31,17 +35,20 @@ public class UserController {
             newUser = user.toBuilder().id(userId++).build();
         }
         users.add(newUser);
+        log.info("Пользователь успешно создан, id = " + newUser.getId() + ". Всего пользователей " + users.size());
         return newUser;
     }
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User user) throws ValidationException {
+        log.info("Получен запрос к эндпоинту: /PUT /users");
         if (user == null) {
             throw new ValidationException("Тело запроса не содержит объекта");
         }
         for (int i = 0; i < users.size(); i++) {
             if (users.get(i).getId() == user.getId()) {
                 users.set(i, user);
+                log.info("Пользователь с id = " + user.getId() + " успешно обновлен.");
                 return user;
             }
         }
