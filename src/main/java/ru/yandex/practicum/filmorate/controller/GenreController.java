959928faed0c914c2f09ce9cc7e761cf7d7c580f
Anatoly Mitapov <aa.mitapov@yandex.ru>
2023-06-07 23:58:38 +0800

    feat: добавить контроллеры mpa и genre

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -0,0 +1,10 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/genres")
+public class GenreController {
+
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/MpaService.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Mpa;
+
+import java.util.List;
+
+@Service
+public class MpaService {
+    public List<Mpa> getAllMpa() {
+
+    }
+
+    public Mpa getMpa(int id) {
+    }
+}
